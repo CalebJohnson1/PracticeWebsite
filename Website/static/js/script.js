7832715aa5bc5fb91920d6aa0dccc2328af3fab1
CalebJohnson1 <Calebgaryj@gmail.com>
2022-04-25 14:02:08 -0700

    Added more functionality to blackjack

--- Website/static/js/script.js
+++ Website/static/js/script.js
@@ -141,8 +141,49 @@ function randomColors() {
 }
 
 // Challenge 5: Black Jack
+let blackjackGame = {
+    'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
+    'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+    'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+};
+
+const YOU = blackjackGame['you']
+const DEALER = blackjackGame['dealer']
+
+const hitSound = new Audio('static/sounds/swish.m4a');
+
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+// document.querySelector('#blackjack-stand-button').addEventListener('click', blackjackStand);
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
 
 function blackjackHit() {
-    alert('Ouch, you just clicked me!');
+    let card = randomCard();
+    showCard(card, YOU);
+    showCard(card, DEALER);
+}
+
+function showCard(card, activePlayer) {
+    let cardImage = document.createElement('img');
+    cardImage.src = `static/images/${card}.png`;
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+    hitSound.play();
+}
+
+function blackjackDeal() {
+    // Selects all images inside of your box
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
+    
+    for (i = 0; i < yourImages.length; i++) {
+        yourImages[i].remove();
+    }
+
+    for (i = 0; i < dealerImages.length; i++) {
+        dealerImages[i].remove();
+    }
+}
+
+function randomCard() {
+    let randomIndex = Math.floor(Math.random() * 13);
+    return blackjackGame['cards'][randomIndex];
 }
